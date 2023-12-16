a622ce5e0c876ed5c4f4485abc6a35457561e526
okiiiiiina <marcy.0623.doi@gmail.com>
2023-12-17 01:58:23 +0900

    fix: eslintrc.jsの編集
     <script setup>タグでimportされたcomponentが<template>未使用として判定されるのを防ぐため
    @vue プラグインを指定

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,15 @@ module.exports = {
     es6: true,
   },
   extends: [
-    // 省略,
     "prettier",
   ],
+  // @vue/eslint-plugin をプラグインとして指定
+  plugins: [
+    '@vue',
+  ],
+  rules: {
+    // <script setup>タグでimportされたcomponentが<template>未使用としてマークされるのを防ぐため追加(↓現在不要かも?)
+    // see: https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/script-setup-uses-vars.md
+    'vue/script-setup-uses-vars': 1,
+  },
 };
