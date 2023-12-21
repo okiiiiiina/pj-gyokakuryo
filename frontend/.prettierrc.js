6e776fdbfc82f43c0e9575a88e471311776e74ab
okiiiiiina <marcy.0623.doi@gmail.com>
2023-12-21 22:19:11 +0900

    add: prettier設定

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false // セミコロンの使用
+  singleQuote: true, // シングルクォートの使用
+  printWidth: 100, // 1行の最大文字数
+  trailingComma: "es5", // 末尾のカンマ
+  tabWidth: 2,
+  vueIndentScriptAndStyle: false, // <script> タグと <style> タグ内のコードインデント設定
+};
