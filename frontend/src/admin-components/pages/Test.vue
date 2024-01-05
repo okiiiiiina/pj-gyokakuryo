67b416613d3cc4e54f8adea3d0fd69f8ac61da2f
okiiiiiina <marcy.0623.doi@gmail.com>
2024-01-05 22:41:17 +0900

    add: 試し書きしやすいようにTest .vue作成

---
+++ frontend/src/admin-components/pages/Test.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import MAccordion from '../../admin-components/general/MAccordion.vue'
+
+
+</script>
+
+<template>
+  <div>
+    <div class="header">
+      <div class="header__right">
+        <div class="title">テスト</div>
+      </div>
+      <div class="header__left">
+        <!-- ヘッダー ユーザアイコン -->
+      </div>
+    </div>
+
+    <MAccordion />
+
+    <div class="body">
+      <div class="pagination"></div>
+      <table class="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <tb></tb>
+            <tb></tb>
+            <tb></tb>
+            <tb></tb>
+          </tr>
+        </tbody>
+      </table>
+      <div class="pagination"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
